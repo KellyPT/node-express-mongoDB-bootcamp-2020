f1f136447c561a741459e483f3abc9353564ccd5
KellyPT <trankphuong@gmail.com>
2020-05-14 16:38:11 -0700

    basic setup for first project; write synchronous code

---
+++ 1-node-farm/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textInput);
+
+const textOutput = `This is what we know about avocado: ${textInput}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync("./txt/output.txt", textOutput);
+
+console.log("File written!");
