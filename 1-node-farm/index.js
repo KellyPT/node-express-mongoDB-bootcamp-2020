34a49183ecd05535e47ef3953c56944c8157771f
KellyPT <trankphuong@gmail.com>
2020-05-14 17:00:14 -0700

    read and write files asynchronously!

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,10 +1,29 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textInput);
+// Blocking, synchronous code
+// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textInput);
 
-const textOutput = `This is what we know about avocado: ${textInput}.\nCreated on ${Date.now()}`;
+// const textOutput = `This is what we know about avocado: ${textInput}.\nCreated on ${Date.now()}`;
 
-fs.writeFileSync("./txt/output.txt", textOutput);
+// fs.writeFileSync("./txt/output.txt", textOutput);
+// console.log("File written!");
 
-console.log("File written!");
+// Non-blocking, asynchronous code
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  console.log('Data1:', data1);
+  console.log('file was read');
+
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log('Data2:', data2);
+
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log('Data3:', data3);
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Your file has been written!');
+      });
+    });
+  });
+});
+
+console.log('will read this file'); // this will run first
