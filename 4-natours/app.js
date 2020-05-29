a3b46dc2a57b4a243e091eabe71405983a92889e
KellyPT <trankphuong@gmail.com>
2020-05-29 13:50:47 -0700

    handle POST requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,15 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.status(200).send('you can post data to this endpoint');
-// });
+app.use(express.json()); // middleware: function that can modify incoming request data
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -26,8 +18,25 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-const port = 3000;
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+});
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
