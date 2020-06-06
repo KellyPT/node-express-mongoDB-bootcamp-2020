d87c72b6a8b73627ba45a3b8cfbf075628f58fde
KellyPT <trankphuong@gmail.com>
2020-06-05 17:43:33 -0700

    connect MongoDB with our Node app

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,8 +1,39 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+// connecting to hosted database on cloud
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log(
+      'Hosted DB connection successful!'
+    );
+  });
+
+// connecting to local database on local machine
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   })
+//   .then(() => {
+//     console.log(
+//       'Local DB connection successful!'
+//     );
+//   });
 
 // 4. START SERVER
 const port = process.env.PORT || 3000;
