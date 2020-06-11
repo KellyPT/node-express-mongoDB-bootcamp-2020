cc114be591d816b10715c6d8baf32510aa01b984
KellyPT <trankphuong@gmail.com>
2020-06-11 16:07:25 -0700

    advanced filtering in request query

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -37,7 +37,14 @@ exports.getAllTours = async (req, res) => {
       (field) => delete queryObj[field]
     );
 
-    const query = Tour.find(queryObj);
+    // Advanced querying
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     const tours = await query;
 
