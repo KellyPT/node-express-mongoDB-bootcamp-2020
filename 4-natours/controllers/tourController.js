c1898fa967961249a1865a8b7b559573957ba874
KellyPT <trankphuong@gmail.com>
2020-06-12 12:06:18 -0700

    create alias for specific API requests

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -8,6 +8,15 @@ const Tour = require('../models/tourModel');
 
 // 2. ROUTE HANDLERS
 
+// middleware for alias cheapest 5 tours
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields =
+    'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // SIMPLE WAY:
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -4,13 +4,21 @@ const {
   createOneTour,
   getOneTour,
   updateOneTour,
-  deleteOneTour
+  deleteOneTour,
+  aliasTopTours
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // middleware to validate params in request URL
 // router.param('id', checkID);
+
+// TIP: http method will take a middleware function if necessary, then controller function
+
+router
+  .route('/top-5-cheap')
+  .get(aliasTopTours, getAllTours);
+// then in POSTMAN, we will run GET 127.0.0.1:3000/api/v1/tours/top-5-cheap
 
 router
   .route('/')
